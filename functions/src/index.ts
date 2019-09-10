88a51fbb2c458f9b9ffaba56f4fe9ca8f92d576c
Jacob Vroegop <j.vroegop@jool.nl>
2019-09-10 16:30:22 +0200

    add functions dir

---
+++ functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+export const createUser = functions.https.onRequest((request, response) => {
+  admin.firestore().doc('users/' + request.body.uid).set({
+      uid: request.body.uid
+    })
+    .then((data) => {console.log('doc succesfully created'); response.send(data);})
+    .catch((err) => {console.log(err); response.status(500).send(err)})
+});
