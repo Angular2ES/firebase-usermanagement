2fb518b33ec8f59641c6376793f8d18f7371a206
Jacob Vroegop <j.vroegop@jool.nl>
2019-09-26 13:49:04 +0200

    testing adding data to group-container

--- src/app/group-list/group-container/group-container.ts
+++ src/app/group-list/group-container/group-container.ts
@@ -1,22 +1,31 @@
 import { Component, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { ValidationService } from 'src/services/validation.service';
-
-interface groupForm {
-  groupId: string;
-  groupName: string;
-}
+import { GroupService } from 'src/services/group.service';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'group-container',
-  template: `{{groupid}}`
+  template: `[{{ groupid | async | json }}] `
 })
 export class GroupContainer implements OnInit{
   @Input('init') groupid: string;
   
-  constructor() { }
-
-  ngOnInit(): void {
+  groupData: FormGroup
+  
+  constructor(private groupService: GroupService) { }
+  
+  ngOnInit(): void {    
+    this.groupService.getGroup(this.groupid).pipe(
+      tap(group => console.log(group)),
+      map(group => {
+        this.groupData = new FormBuilder().group({
+          groupId: this.groupid,
+          groupName: group.groupName,
+          users: group.users
+        })        
+      })
+    )
     console.log(this.groupid);
   }
 }
