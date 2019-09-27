bb04b7b9483ec6ba39468d5532f6d8237009ea8e
Jacob Vroegop <j.vroegop@jool.nl>
2019-09-27 15:53:40 +0200

    update onDelete, user removed from all the groups

--- functions/src/user/user.module.ts
+++ functions/src/user/user.module.ts
@@ -1,8 +1,15 @@
 import admin = require("firebase-admin");
+import { FieldValue } from "@google-cloud/firestore";
+
+export interface UserModel {
+  uid: string;
+  name: string;
+  groups: string[];
+}
 
 export function onCreateUser(user: admin.auth.UserRecord ) {
   admin.firestore().doc(`users/${user.uid}`).set({
-    uid: user.uid
+    uid: user.uid,
   })
   .then(() => {
     admin.firestore().doc(`user_private_profile/${user.uid}`).set({
@@ -15,6 +22,23 @@ export function onCreateUser(user: admin.auth.UserRecord ) {
 };
 
 export function onDeleteUser(user: admin.auth.UserRecord) {
+  
+  // Remove this user from all the groups
+  admin.firestore().doc(`users/${user.uid}`).get()
+  .then((snap) => {
+    if (snap.data()){
+      const batch = admin.firestore().batch();
+      snap.get('groups').forEach((groupId: string) => {
+        const groupRef = admin.firestore().doc(`groups/${groupId}`); //update(groupUpdate).catch((e) => console.log(e));
+        batch.update(groupRef, {'users': FieldValue.arrayRemove(snap.get('uid'))})
+      });
+      batch.commit()
+      .then(() => console.log('succes'))
+      .catch((e) => console.log(e))
+    }
+  }).catch((e) => console.log(e));
+
+  // Delete the user from database
   admin.firestore().doc(`users/${user.uid}`).delete()
   .then(() => {
     admin.firestore().doc(`user_private_profile/${user.uid}`).delete()
@@ -23,5 +47,7 @@ export function onDeleteUser(user: admin.auth.UserRecord) {
     }).catch((err) => console.log(err))
   })
   .catch((err) => console.log(err))
+
+  return Promise.resolve();
 }
 
