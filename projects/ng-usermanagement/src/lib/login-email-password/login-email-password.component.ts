1f4d46d1df17a7a727ace2efb8384677dd43b73a
Jacob Vroegop <j.vroegop@jool.nl>
2019-10-28 10:47:36 +0100

    generated a library

---
+++ projects/ng-usermanagement/src/lib/login-email-password/login-email-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-login-email-password',
+  templateUrl: './login-email-password.component.html',
+  styleUrls: ['./login-email-password.component.css']
+})
+export class LoginEmailPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ng-usermanagement/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-usermanagement
+ */
+
+export * from './lib/login-email-password/login-email-password.module';
+export * from './lib/login-email-password/login-email-password.component';
