7d69d68fb8cde8b67841ef2c9151ff734030eadd
Jacob Vroegop <j.vroegop@jool.nl>
2019-11-05 10:29:47 +0100

    imports organizing

--- apps/firebase-usermanagement/src/app/app.module.ts
+++ apps/firebase-usermanagement/src/app/app.module.ts
@@ -1,45 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { AppHomeComponent } from './app-home/app-home.component';
-import { LoginComponent } from './login-email-password/email-password-login.component';
-import { HeaderComponent } from './header/header.component';
-import { LoginTestComponent } from './login.test.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
-import { LoginGoogleComponent } from './login-google/login-google.component';
-import { UserSettingsTestComponent } from './user.settings.test.component';
-import { GroupSettingsComponent } from './group-settings/group-settings.component';
-import { ValidationMessagesComponent } from './validation-messages/validation-messages.component';
-import { GroupListComponent } from './group-list/group-list.component';
-import { GroupListContainerComponent } from './group-list/group-list-container/group-list-container.component';
-import { AppComponent } from './app.component';
-
-import { UserModuleConfig } from '../users-module-config';
-
 // firebase imports
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StorageModule } from '@ngx-pwa/local-storage';
+import { MaterializeModule } from 'angular2-materialize';
+import { ToasterModule, ToasterService } from 'angular2-toaster';
+import { LibModule } from 'libs/ng-usermanagement/src/public-api';
 import { environment } from '../environments/environment';
-
 // services
 import { AuthenticationService } from '../services/auth.service';
-import { UserAuthGuardService } from '../services/auth.services/user-auth-guard.service';
 import { AdminAuthGuardService } from '../services/auth.services/admin-auth-guard.service';
-
+import { GroupAdminAuthGuardService } from '../services/auth.services/groupAdmin-auth-gaurd.service';
+import { UserAuthGuardService } from '../services/auth.services/user-auth-guard.service';
+import { UserModuleConfig } from '../users-module-config';
+import { AppHomeComponent } from './app-home/app-home.component';
 // modules
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterializeModule } from 'angular2-materialize';
-import { UserSettingsComponent } from './user-settings/user.settings.Component';
+import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { GroupListContainerComponent } from './group-list/group-list-container/group-list-container.component';
+import { GroupListComponent } from './group-list/group-list.component';
+import { GroupSettingsComponent } from './group-settings/group-settings.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login-email-password/email-password-login.component';
 import { LoginEmailComponent } from './login-email/login-email.component';
-import { StorageModule } from '@ngx-pwa/local-storage';
-import { ToasterService, ToasterModule } from 'angular2-toaster';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginGoogleComponent } from './login-google/login-google.component';
+import { LoginTestComponent } from './login.test.component';
 import { UserListContainerComponent } from './user-list-container/user-list-container.component';
-import { GroupAdminAuthGuardService } from '../services/auth.services/groupAdmin-auth-gaurd.service';
-import { LibModule } from 'libs/ng-usermanagement/src/public-api';
+import { UserSettingsComponent } from './user-settings/user.settings.Component';
+import { UserSettingsTestComponent } from './user.settings.test.component';
+import { ValidationMessagesComponent } from './validation-messages/validation-messages.component';
 
 // TODO remove this after refactoring
 const userModuleConfig: UserModuleConfig = {
@@ -81,7 +76,6 @@ const userModuleCfg = {
     MaterializeModule,
     BrowserAnimationsModule,
     ToasterModule.forRoot(),
-    
     
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
