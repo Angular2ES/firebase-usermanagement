c21791e46d9285dfbcf6cdb8fdf86b1ff93d9fa5
Jacob Vroegop <j.vroegop@jool.nl>
2020-01-10 14:33:14 +0100

    logout test in user settings

--- cypress/integration/settings/user-settings.spec.js
+++ cypress/integration/settings/user-settings.spec.js
@@ -20,4 +20,12 @@ context('Actions', () => {
       expect(str).to.equal(`update succesful`)
     })    
   })
+
+  it('logout user', () => {
+    cy.get('[style="float: right;"]').click()
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`logout succesful`)
+    }) 
+  })
 })
