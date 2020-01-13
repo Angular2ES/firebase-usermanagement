10ab33eb2032ac43f4522b8be465edfb99d9aabe
Jacob Vroegop <j.vroegop@jool.nl>
2020-01-13 14:17:59 +0100

    fix admin auth guard

--- libs/ng-usermanagement/src/lib/guards/admin-auth-guard.service.ts
+++ libs/ng-usermanagement/src/lib/guards/admin-auth-guard.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { MatSnackBar } from '@angular/material';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
-import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class AdminAuthGuardService implements CanActivate {
 
-  constructor(private snackBar: MatSnackBar) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private snackBar: MatSnackBar
+    ) {
   }
 
-  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    try {
-      const idTokenResult = await firebase.auth().currentUser.getIdTokenResult()
-      if (idTokenResult.claims.admin){
-        return true;
-      } else {
-        this.snackBar.open('insufficient permission', '', { duration: 2000 })
-        return false;
-      }
-    } catch {
-      this.snackBar.open('insufficient permission', '', { duration: 2000 })
-      return false;
-    }
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.afAuth.user.pipe(
+      switchMap(async user => {
+        try {
+          const idTokenResult = await user.getIdTokenResult()
+          if (idTokenResult.claims.admin){
+            return true;
+          } else {
+            this.snackBar.open('insufficient permission', '', { duration: 2000 })
+            return false;
+          }
+        }
+        catch {
+          this.snackBar.open('insufficient permission', '', { duration: 2000 })
+          return false;
+        }
+      })
+    )
   }
 }
