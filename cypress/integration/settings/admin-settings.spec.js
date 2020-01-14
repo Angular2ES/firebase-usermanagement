39f40c54a8ba1642f3cf6bae3ac06e98d01fafbf
Jacob Vroegop <j.vroegop@jool.nl>
2020-01-14 10:04:41 +0100

    removed wait function in e2e test

--- cypress/integration/settings/admin-settings.spec.js
+++ cypress/integration/settings/admin-settings.spec.js
@@ -5,9 +5,10 @@ context('Actions', () => {
   })
 
   it('update user settings', () => {
-    cy.visit('http://localhost:4200/adminSettings').wait(3000)
-
-    cy.get('.admin-settings-collection > :nth-child(1) > :nth-child(3)').click()
+    cy.visit('http://localhost:4200/adminSettings')
+    
+    cy.get('.admin-settings-collection').contains('jacob@jo.com')
+    cy.get(':nth-child(1) > [name="EditUserSettings"]').click()
 
     cy.get('.form-container > .ng-untouched')
       .clear()
@@ -18,17 +19,20 @@ context('Actions', () => {
     cy.on('window:alert', (str) => {
       expect(str).to.equal(`update succesful`)
     })
-    cy.get('[style="right: 10px; position: absolute;"]').click()
+    cy.get('.close-button').contains('Close').click()
   })
 
   it('impersonate user', () => {
-    cy.visit('http://localhost:4200/adminSettings').wait(3000)
-
-    cy.get('.admin-settings-collection > :nth-child(1) > :nth-child(4)').click().wait(2000)
+    cy.visit('http://localhost:4200/adminSettings')    
+    
+    cy.get('.admin-settings-collection').contains('jacob@jo.com')
+    cy.get(':nth-child(1) > [name="ImpersonateUser"]').click()
 
     cy.on('window:alert', (str) => {
       expect(str).to.equal(`login succesful`)
     })
+
+    cy.get('.admin-popup')
 
     cy.visit('http://localhost:4200/home')
 
