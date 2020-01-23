d00de195b602ee21167b07d1dc6bd7c82f5e68e3
Jacob Vroegop <j.vroegop@jool.nl>
2020-01-23 11:38:06 +0100

    removed catchError because it stopped the stream

--- libs/ng-usermanagement/src/lib/guards/admin-auth-guard.service.ts
+++ libs/ng-usermanagement/src/lib/guards/admin-auth-guard.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { MatSnackBar } from '@angular/material';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
-import { Observable, from, of } from 'rxjs';
-import { switchMap, map, tap, catchError, retry } from 'rxjs/operators';
+import { Observable, from, of, asyncScheduler, defer, interval, race } from 'rxjs';
+import { switchMap, map, tap, catchError, retry, mergeMap, distinctUntilChanged, debounce, flatMap } from 'rxjs/operators';
 
 @Injectable()
 export class AdminAuthGuardService implements CanActivate {
@@ -14,27 +14,48 @@ export class AdminAuthGuardService implements CanActivate {
     ) {
   }
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {''
     return this.afAuth.user.pipe(
+      distinctUntilChanged(),
       switchMap(user => {
-        if (user === null) { 
-          throw new Error();
-        }
-        return from(user.getIdTokenResult()).pipe(
-          map(idTokenResult => idTokenResult.claims.admin),
-          tap((isAdmin: boolean) => { 
+        // Keeping the main observable stream alive by creating a disposable stream
+        const disposableStream$ = of(user);
+        return disposableStream$.pipe(
+          // Timing of the observable can be an issue so we wait a second for the value stream to update
+          debounce(user => { 
+            if (user === null) {
+              return interval(1000);
+            } else {
+              return interval(0);
+            }
+          }),
+          flatMap((user) => {
+
+            const notAnAdminPromise = Promise.resolve({ 
+              claims: {
+                admin: false
+              }
+            });
+
+            // If the user is still null or the promise is rejected we return a admin user with the value of false 
+            if (user === null) {
+              return notAnAdminPromise;
+            } else {
+              return user.getIdTokenResult().catch(() => {
+                return notAnAdminPromise;
+              });
+            }
+          }),
+          map((IdTokenResult) => { 
+            // If the user is not an admin a snackbar is shown
+            const isAdmin = IdTokenResult.claims.admin;
             if (!isAdmin) {
               this.snackBar.open('insufficient permission', '', { duration: 2000 }); 
-            } 
-            return of(isAdmin)
+            }
+            return isAdmin;
           })
-          )
-      }),
-      retry(2),
-      catchError((err, source) => { 
-        this.snackBar.open('insufficient permission', '', { duration: 2000 }); 
-        return of(false)
-      }),
+        )
+      })
     )
   }
 }
